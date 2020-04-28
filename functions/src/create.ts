8e4b713f2c61124f685b1bea15f1865be9547a4b
Jason Heron <jheron@google.com>
2020-04-28 16:57:03 -0700

    fix the request type

--- functions/src/create.ts
+++ functions/src/create.ts
@@ -4,46 +4,34 @@ import * as express from 'express'
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors'
 
-// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
-// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
-// `Authorization: Bearer <Firebase ID Token>`.
-// when decoded successfully, the ID Token content will be added as `req.user`.
-const validateFirebaseIdToken = async (req: any, res: any, next: any) => {
-  console.log('Check if request is authorized with Firebase ID token');
+interface Request extends express.Request {
+  user?: admin.auth.DecodedIdToken;
+}
 
+const validateFirebaseIdToken = async (req: Request, res: express.Response, next: any) => {
   if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
       !(req.cookies && req.cookies.__session)) {
-    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
-        'Make sure you authorize your request by providing the following HTTP header:',
-        'Authorization: Bearer <Firebase ID Token>',
-        'or by passing a "__session" cookie.');
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.');
     res.status(403).send('Unauthorized');
     return;
   }
 
   let idToken;
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-    console.log('Found "Authorization" header');
-    // Read the ID Token from the Authorization header.
     idToken = req.headers.authorization.split('Bearer ')[1];
   } else if(req.cookies) {
-    console.log('Found "__session" cookie');
-    // Read the ID Token from cookie.
     idToken = req.cookies.__session;
   } else {
-    // No cookie
     res.status(403).send('Unauthorized');
     return;
   }
 
   try {
     const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-    console.log('ID Token correctly decoded', decodedIdToken);
     req.user = decodedIdToken;
     next();
     return;
   } catch (error) {
-    console.error('Error while verifying Firebase ID token:', error);
     res.status(403).send('Unauthorized');
     return;
   }
@@ -55,8 +43,8 @@ app.use(cors({origin: true}));
 app.use(cookieParser());
 app.use(validateFirebaseIdToken);
 
-app.post('*', (req, res) => {
-  res.send(`Hello ${(req as any).user.name}`);
+app.post('*', (req: Request, res: express.Response) => {
+  res.send(`Hello ${req.user!.name}`);
 });
 
 // This HTTPS endpoint can only be accessed by your Firebase Users.
