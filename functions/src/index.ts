d3a9dffd4bb1a2a6d507493d4e6314e5db25a1ad
Jason Heron <jheron@google.com>
2020-04-28 10:54:27 -0700

    adding functions for viewing and creating a menu

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,6 @@
-import * as functions from 'firebase-functions';
+import {createMenu} from './create';
+import {viewMenu} from './view';
+export const c = createMenu;
+export const v = viewMenu;
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
