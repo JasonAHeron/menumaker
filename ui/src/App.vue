7f0a9d66e9a430c0d6bd4bb6e178289a08ffe3c1
Jason Heron <jheron@google.com>
2020-04-28 15:00:19 -0700

    add initial UI component

---
+++ ui/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div v-if="!authenticated">
+      <button @click="login">Login</button>
+    </div>
+    <div v-if="authenticated">
+      <button @click="logout">Logout</button>
+      <h1>Hi {{ firstName }}!</h1>
+      <button @click="createMenu">Create a menu!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Firebase from "./firebase.js";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      user: {
+        loggedIn: false,
+        data: {}
+      }
+    };
+  },
+  computed: {
+    authenticated() {
+      return this.user.loggedIn;
+    },
+    firstName() {
+      if (this.user.data.displayName) {
+        return this.user.data.displayName;
+      }
+      return null;
+    }
+  },
+  methods: {
+    login() {
+      Firebase.login();
+    },
+    logout() {
+      Firebase.logout();
+    },
+    createMenu() {
+      Firebase.createMenu().then(result => {
+        console.log(result);
+      })
+    }
+  },
+  mounted: function() {
+    Firebase.auth.onAuthStateChanged(user => {
+      if (user) {
+        this.user.loggedIn = true;
+        this.user.data = user;
+      } else {
+        this.user.loggedIn = false;
+        this.user.data = {};
+      }
+    });
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ui/src/firebase.js
@@ -0,0 +1,42 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/functions'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFMsC2NH6DRMp9fjX_ko4ftHvjc0FN5UE",
+  authDomain: "menumaker-b4bf2.firebaseapp.com",
+  databaseURL: "https://menumaker-b4bf2.firebaseio.com",
+  projectId: "menumaker-b4bf2",
+  storageBucket: "menumaker-b4bf2.appspot.com",
+  messagingSenderId: "1090297600118",
+  appId: "1:1090297600118:web:b175ca8cc94bf83821e757"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default {
+  auth: firebase.auth(),
+  login() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+      console.log(result);
+    })
+    .catch(function(error){
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+      console.log(errorCode, errorMessage, email, credential);
+      })
+  },
+  logout() {
+    firebase.auth().signOut()
+    .then(function() {})
+    .catch(function(error) {
+      console.log(error)});
+  },
+  createMenu() {
+    return firebase.functions().httpsCallable('c').call();
+  }
+}
