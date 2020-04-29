6689b4198db0124fbf485e97a10bc1c3c645f61e
Jason Heron <jheron@google.com>
2020-04-29 16:51:02 -0700

    trying to add views but failing

--- functions/src/view.ts
+++ functions/src/view.ts
@@ -1,34 +1,40 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express'
 import * as cors from 'cors'
-import {SHEETS_KEY} from './keys';
-import {google} from 'googleapis';
+import * as handlebars from 'express-handlebars';
 
-const TEST_SHEET = '18zMmVFakNS_FAWSegEKB9UypHKpCnC8fmiPYMgXuN3Y'
-const HEADER_RANGE = 'Sheet1!A2:D2';
-const DATA_RANGE = 'Sheet1!A4:C100';
+// import {SHEETS_KEY, TEST_SHEET} from './keys';
+// import {google} from 'googleapis';
+
+// const HEADER_RANGE = 'Sheet1!A2:D2';
+// const DATA_RANGE = 'Sheet1!A4:C100';
 
 const app = express();
 app.use(cors({origin: true}));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
 
-const sheets = google.sheets({
-  version: 'v4',
-  auth: SHEETS_KEY,
-});
+// const sheets = google.sheets({
+//   version: 'v4',
+//   auth: SHEETS_KEY,
+// });
 
 app.get('*', (req: express.Request, res: express.Response) => {
-  sheets.spreadsheets.values.batchGet({
-    spreadsheetId: TEST_SHEET, ranges: [HEADER_RANGE, DATA_RANGE]
-  }).then(result => {
-    if (result.data.valueRanges?.[0]?.values && result.data.valueRanges?.[1]?.values) {
-      const header_values = result.data.valueRanges[0].values;
-      const data_values = result.data.valueRanges[1].values;
-      res.send({header_values, data_values});
-    }
-    res.send("Failed to fetch values for expected ranges");
-  }).catch(error => {
-    res.send(error);
-  })
+  // sheets.spreadsheets.values.batchGet({
+  //   spreadsheetId: TEST_SHEET, ranges: [HEADER_RANGE, DATA_RANGE]
+  // }).then(result => {
+  //   if (result.data.valueRanges?.[0]?.values && result.data.valueRanges?.[1]?.values) {
+  //     //const header_values = result.data.valueRanges[0].values;
+  //     //const data_values = result.data.valueRanges[1].values;
+  //     //res.send({header_values, data_values});
+  //     res.render('main');
+  //   }
+  //   res.send("Failed to fetch values for expected ranges");
+  // }).catch(error => {
+  //   res.render('main');
+  //   //res.send(error);
+  // })
+  res.render('home');
 });
 
 export const viewMenu = functions.https.onRequest(app);
