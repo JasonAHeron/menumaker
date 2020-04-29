56d7337475e29521d2a792ca3c9e4f84a1d4108a
Jason Heron <jheron@google.com>
2020-04-29 10:02:10 -0700

    connect sheets api for view mode with just api key

--- functions/src/view.ts
+++ functions/src/view.ts
@@ -1,5 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express'
+import * as cors from 'cors'
+import {SHEETS_KEY} from './keys';
+import {google} from 'googleapis';
 
-export const viewMenu = functions.https.onRequest((request, response) => {
-  response.send("View a menu");
- });
+const TEST_SHEET = '1VfjfNv2IqW7_VYa0kCCoToQLz1B3tFvnIRmT5czwotI'
+const app = express();
+app.use(cors({origin: true}));
+const sheets = google.sheets({
+  version: 'v4',
+  auth: SHEETS_KEY,
+});
+
+app.get('*', (req: express.Request, res: express.Response) => {
+  sheets.spreadsheets.get({spreadsheetId: TEST_SHEET}).then(result => {
+    res.send(result.data);
+  }).catch(error => {
+    res.send(error);
+  })
+});
+
+export const viewMenu = functions.https.onRequest(app);
