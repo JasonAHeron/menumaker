d5dd088ee0a481c467ac02c14332f00002e8aab8
Rohan Dandavati <dandavati@google.com>
2020-04-29 12:41:08 -0700

    cors errors

--- functions/src/create.ts
+++ functions/src/create.ts
@@ -2,7 +2,9 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express'
 import * as cookieParser from 'cookie-parser';
-import * as cors from 'cors'
+import * as cors from 'cors';
+import {SHEETS_KEY} from './keys';
+import {google} from 'googleapis';
 
 interface Request extends express.Request {
   user?: admin.auth.DecodedIdToken;
@@ -37,14 +39,42 @@ const validateFirebaseIdToken = async (req: Request, res: express.Response, next
   }
 };
 
+// const sheets = google.sheets({
+//   version: 'v4',
+//   auth: SHEETS_KEY,
+// });
+
+const drive = google.drive({
+  version: 'v3',
+  auth: SHEETS_KEY,
+});
+
+const TEST_SHEET = '18zMmVFakNS_FAWSegEKB9UypHKpCnC8fmiPYMgXuN3Y'
+
 admin.initializeApp();
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors({origin: false}));
 app.use(cookieParser());
 app.use(validateFirebaseIdToken);
 
 app.post('*', (req: Request, res: express.Response) => {
-  res.send({data: {user: req.user!.name, sheet: "1234", menu: "567"}});
-});
+  drive.files.copy({fileId: TEST_SHEET}).then(response => {
+    res.send({data: response.data});
+  }).catch(error => {
+    // Handle the error
+    console.log(error);
+    express.response.status(500).send(error);
+  })
+})
+//   const sheetData = {user: req.user!.name,
+//     menuTuple: { sheetsId: "abc", menuId: "def"}
+//   };
+//   admin.firestore().collection('users').add(sheetData).then(() => {
+//     res.send({data: {user: req.user!.name, sheet: "1234", menu: "567"}});
+// }).catch(error => {
+//   // Handle the error
+//   console.log(error);
+//   express.response.status(500).send(error);
+// }
 
 export const createMenu = functions.https.onRequest(app);
