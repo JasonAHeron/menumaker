221349c35ce4be84ce6faba3c91e99bb175b08f5
Rohan Dandavati <dandavati@google.com>
2020-04-30 12:07:05 -0700

    brokan

--- functions/src/create.ts
+++ functions/src/create.ts
@@ -41,27 +41,46 @@ app.use(cors({ origin: true }));
 app.use(cookieParser());
 app.use(validateFirebaseIdToken);
 app.use(authenticateDrive);
+const db = admin.firestore()
 
 app.post('*', (req: Request, res: express.Response) => {
-  req.drive!.files.copy({ fileId: TEST_SHEET }).then(response => {
-    if (!!response.data?.id) {
-      req.drive!.permissions.create({
-        fileId: response.data.id,
-        requestBody: { type: 'user', role: 'writer', emailAddress: req.user!.email }
-      }).then(() => {
-        const userMenuId = response.data.id!.substring(0, 6)
-        const p1 = admin.firestore().collection('menuIds').doc(userMenuId).set({ spreadsheetId: response.data.id });
-        const p2 = admin.firestore().collection('users').doc(req.user!.name).set({ menuId: userMenuId, spreadsheetId: response.data.id });
-        Promise.all([p1, p2])
-        .then(() => res.send({ data: { sheet: response.data.id, menu: userMenuId } }))
-        .catch(err => console.error(err));
-      }).catch(err => console.error(err));
-    }
-  })
-  .catch(error => {
-    console.error(error);
-    res.status(500).send(error);
-  });
+  if(!!req.user?.email) {
+    db.collection('users').doc(req.user.email).get()
+    .then(doc => {
+      if (!doc.exists) {
+        req.drive!.files.copy({ fileId: TEST_SHEET }).then(response => {
+          if (!!response.data?.id) {
+            req.drive!.permissions.create({
+              fileId: response.data.id,
+              requestBody: { type: 'user', role: 'writer', emailAddress: req.user!.email }
+            }).then(() => {
+              const userMenuId = response.data.id!.substring(0, 6)
+              const p1 = db.collection('menuIds').doc(userMenuId).set({ spreadsheetId: response.data.id });
+              const p2 = db.collection('users').doc(req.user!.email!).set({ menuId: userMenuId, spreadsheetId: response.data.id });
+              Promise.all([p1, p2])
+              .then(() => {
+                res.send({ data: { sheet: response.data.id, menu: userMenuId }});
+                return;
+              })
+              .catch(err => console.error(err));
+            }).catch(err => console.error(err));
+          }
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).send(error);
+          return;
+        });
+      } else {
+        res.send({data: {sheet: doc.get('spreadsheetId'), menu: doc.get('menuId')}});
+        return;
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    })
+  }
+  res.status(500).send();
 });
 
 export const createMenu = functions.https.onRequest(app);
